fd7387189211254eef6a7ed5a959c5f76049e4f3
Katharinaad <vermilion.adams@gmail.com>
2023-10-06 22:41:06 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,11 +15,27 @@ export class App extends Component {
     }));
   };
 
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+  countPositiveFeedbackPercentage() {
+    return (this.good.state / this.countTotalFeedback()) * 100;
+  }
+
   render() {
+    const { good, neutral, bad, feedbackGiven } = this.state;
+
     return (
-      <div>
-        <h1>Feedback App</h1>
-      </div>
+      <>
+        <h2>Feedback App</h2>
+        <button onClick={() => this.handleFeedback('good')}>Good</button>
+        <button onClick={() => this.handleFeedback('neutral')}>Neutral</button>
+        <button onClick={() => this.handleFeedback('bad')}>Bad</button>
+        <h2>Statistics</h2>
+        <span>Good: {good}</span>
+        <span>Neutral: {neutral}</span>
+        <span>Bad: {bad}</span>
+      </>
     );
   }
 }
