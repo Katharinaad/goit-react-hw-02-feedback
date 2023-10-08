5f0dae993371e53ce32d2366c8f265a0e7ab0777
Katharinaad <vermilion.adams@gmail.com>
2023-10-08 13:45:01 +0200

    Create Statistics.jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,14 @@
+export function Statistics({ good, neutral, bad, total, positivePercentage }) {
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>
+        Positive feedback:
+        {Math.round({ positivePercentage })}%
+      </p>
+    </div>
+  );
+}
